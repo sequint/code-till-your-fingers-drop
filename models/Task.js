2f57bb9a86aeb93243f769f1db8ac20276e8a5e3
Steven Quintana <steve.e.quint@gmail.com>
2021-09-20 16:56:25 -0700

    Build task model

---
+++ models/Task.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize')
+const { Project } = require('./')
+const sequelize = require('../db')
+
+class Task extends Model {}
+
+Task.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  isComplete: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  },
+  projectId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Project,
+      key: 'id'
+    }
+  }
+}, { sequelize, modelName: 'task' })
+
+module.exports = Task
