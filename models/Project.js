59054fa0cf5c6fddd17f3445407d641cf155e960
Steven Quintana <steve.e.quint@gmail.com>
2021-09-20 16:48:48 -0700

    Create Project model to hold projects data

---
+++ models/Project.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize')
+const { Category } = require('./')
+const sequelize = require('../db')
+
+class Project extends Model {}
+
+// Create a date variable using day, month, and year joined in a string.
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0');
+var yyyy = today.getFullYear();
+
+// Re-assign today to be full date.
+today = mm + '/' + dd + '/' + yyyy;
+
+Project.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  projectName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  startDate: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: today
+  },
+  percentComplete: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0
+  },
+  categoryID: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    references: {
+      model: Category,
+      key: 'id'
+    }
+  }
+}, { sequelize, modelName: project })
+
+module.extends = Project
