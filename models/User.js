38a30f007f995a74045f809d50626557959766bf
Steven Quintana <steve.e.quint@gmail.com>
2021-09-20 16:05:49 -0700

    Build user model with category references join for users interests array of objects

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize')
+const { Category } = require('./')
+const sequelize = require('../db')
+
+class User extends Model {}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  interests: [{
+    type: DataTypes.STRING,
+    allowNull: true,
+    references: {
+      model: Category,
+      key: 'id'
+    }
+  }],
+  age: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+}, { sequelize, modelName: 'user'})
+
+module.exports = User
