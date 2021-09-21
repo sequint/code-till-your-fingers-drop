87e753bab0ac1e8122422ec97b284bc84b4b79ed
Steven Quintana <steve.e.quint@gmail.com>
2021-09-20 17:33:44 -0700

    Import and export all model files

---
+++ models/index.js
@@ -0,0 +1,19 @@
+// Import all models from the folder in order to export them from one file.
+const Category = require('./Category')
+const Comment = require('./Comment')
+const Interest = require('./Interest')
+const Project = require('./Project')
+const Task = require('./Task')
+const User = require('./User')
+
+// Join tables with a foreignKey value.
+
+// Export all models in an object.
+module.exports = {
+  Category,
+  Comment,
+  Interest,
+  Project,
+  Task,
+  User
+}
