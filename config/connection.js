d03a903a8d74ed2b083157b18df3e3a2b56d422f
Steven Quintana <steve.e.quint@gmail.com>
2021-09-21 14:20:20 -0700

    Update database name

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
-const Sequelize = require('sequelize')
+const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.JAWSDB_URL || `mysql://root:${process.env.PASSWORD}@localhost:3306/ecommerce_db`)
+const sequelize = new Sequelize(process.env.JAWSDB_URL || `mysql://root:${process.env.PASSWORD}@localhost:3306/codefingers_db`)
 
 module.exports = sequelize
