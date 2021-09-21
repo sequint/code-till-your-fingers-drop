708bae5c03b815768a1ed3bffbd0729fdf17be3a
Steven Quintana <steve.e.quint@gmail.com>
2021-09-21 13:41:11 -0700

    Change connection file import

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize')
 const { Category } = require('./')
-const sequelize = require('../db')
+const sequelize = require('../config/connection')
 
 class User extends Model {}
 
