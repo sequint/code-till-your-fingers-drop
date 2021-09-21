a7ef1ef3235bcb5acbb27d4c72fcde933d594ee0
Steven Quintana <steve.e.quint@gmail.com>
2021-09-21 14:19:31 -0700

    Delete db index file, replaced with connection file

--- db/index.js
+++
@@ -1,8 +0,0 @@
-// // Imports
-// const { Sequelize } = require('sequelize')
-// require('dotenv').config()
-
-// // Export a new sequelize database connection.
-// // Use jaws database url for connection if it's available.
-// // If jaws is not available, use local db URL path.
-// module.exports = new Sequelize(process.env.JAWSDB_URL || `mysql://root:${process.env.PASSWORD}@localhost:3306/codefingers_db`)
