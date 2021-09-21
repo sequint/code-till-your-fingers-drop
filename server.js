c55191d9828c4f243b123211863dcf5756f0c897
Steven Quintana <steve.e.quint@gmail.com>
2021-09-21 13:41:50 -0700

    Update server to new connection file sync

--- server.js
+++ server.js
@@ -1,44 +1,15 @@
-// Import .env library for passwords.
-require('dotenv').config()
-
-// Import node packages required for project.
 const express = require('express')
-const { join } = require('path')
-const passport = require('passport')
-// const { User, Post } = require('./models')
-const { Strategy: JWTStrategy, ExtractJwt } = require('passport-jwt')
+// import sequelize connection
+const sequelize = require('./config/connection')
 
-// Name app variable to hold express router functions.
 const app = express()
 
-// Use express to join public folder with the server.
-app.use(express.static(join(__dirname, 'public')))
-app.use(express.urlencoded({ extended: true }))
-// Use json with express routes in order to recieve and send data.
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-// Initialize passport for encyption of password and user tokens.
-// app.use(passport.initialize())
-// app.use(passport.session())
-
-// passport.use(User.createStrategy())
-
-// passport.serializeUser(User.serializeUser())
-// passport.deserializeUser(User.deserializeUser())
-
-// Get the token of the user from localStorage, then sign in user if it is available.
-// Join user id with post.
-// passport.use(new JWTStrategy({
-//   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-//   secretOrKey: process.env.SECRET
-// }, ({ id }, cb) => User.findOne({ where: { id }, include: [Post] })
-//   .then(user => cb(null, user))
-//   .catch(err => cb(err, null))))
-
-// Use and import routes folder.
 app.use(require('./routes'))
 
-// Sync the database then turning on our server.
-require('./db')
-  .sync()
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({})
   .then(() => app.listen(process.env.PORT || 3000))
+  .catch(err => console.log(err))
