92671420d29029d04b610ad35c2f2c80e44f5d9c
Steven Quintana <steve.e.quint@gmail.com>
2021-09-22 15:08:05 -0700

    Uncomment remaining routers

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ const router = require('express').Router()
 
 // Declare end points for api routes.
 router.use('/api', require('./categoryRoutes'))
-// router.use('/api', require('./commentRoutes'))
+router.use('/api', require('./commentRoutes'))
 router.use('/api', require('./interestRoutes'))
 router.use('/api', require('./projectRoutes'))
-// router.use('/api', require('./taskRoutes'))
+router.use('/api', require('./taskRoutes'))
 router.use('/api', require('./userRoutes'))
 
 // Export the router file to use elsewhere as 'router'.
