058dbf769c9bc496e7ab3036fbae726b9587599d
Steven Quintana <steve.e.quint@gmail.com>
2021-09-21 17:13:18 -0700

    Create post route

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,42 +4,44 @@ const { Category } = require('../models')
 // Get route - retrieves category data from the database and sends back to front-end.
 router.get('/categories', (req, res) => {
   Category.findAll()
-    .then(categories => res.json(
-      {
+    .then(categories => res.json({
         status: 200,
         categories: categories
-      }
-    ))
-    .catch(err => res.json(
-      {
+      }))
+    .catch(err => res.json({
         status: 400,
         err: err
-      }
-    ))
+      }))
 })
 
 // Get by id route - retreives one data by it's id from the front-end.
 router.get('/categories/:id', (req, res) => {
   // Get the category who's id matches the parameter id passed in.
   Category.findAll({
-    include: product,
     where: { id: req.params.id }
   })
-    .then(category => res.json(
-      {
+    .then(category => res.json({
         status: 200,
         category: category
-      }
-    ))
-    .catch(err => res.json(
-      {
+      }))
+    .catch(err => res.json({
         status: 400,
         err: err
-      }
-    ))
+      }))
 })
 
 // Post route - receives new category data from the front-end and creates new row in the database.
+router.post('/categories', (req, res) => {
+  Category.create(req.body)
+    .then(category => res.json({
+      status: 200,
+      category: category
+    }))
+    .catch(err => res.json({
+      status: 400,
+      err: err
+    }))
+})
 
 // Put route - receives category data from front-end and updates the row in the database with a matching id.
 
