e639a6f3b24eefe9f309bd115026d3d21763485b
Steven Quintana <steve.e.quint@gmail.com>
2021-09-21 17:00:50 -0700

    Create id specific get route and update catches

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,14 +4,40 @@ const { Category } = require('../models')
 // Get route - retrieves category data from the database and sends back to front-end.
 router.get('/categories', (req, res) => {
   Category.findAll()
-    .then(categories => res.json({
-      status: 200,
-      categories: categories
-    }))
-    .catch(err => console.log(err))
+    .then(categories => res.json(
+      {
+        status: 200,
+        categories: categories
+      }
+    ))
+    .catch(err => res.json(
+      {
+        status: 400,
+        err: err
+      }
+    ))
 })
 
 // Get by id route - retreives one data by it's id from the front-end.
+router.get('/categories/:id', (req, res) => {
+  // Get the category who's id matches the parameter id passed in.
+  Category.findAll({
+    include: product,
+    where: { id: req.params.id }
+  })
+    .then(category => res.json(
+      {
+        status: 200,
+        category: category
+      }
+    ))
+    .catch(err => res.json(
+      {
+        status: 400,
+        err: err
+      }
+    ))
+})
 
 // Post route - receives new category data from the front-end and creates new row in the database.
 
