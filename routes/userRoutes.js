36034eee514578aca8107eea41b9f6d4457d2bb7
Steven Quintana <steve.e.quint@gmail.com>
2021-09-21 18:08:35 -0700

    Create get user by id route and create post route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,40 @@
 const router = require('express').Router()
 const { User } = require('../models')
 
-// Get route
+// Get all users.
 router.get('/users', (req, res) => {
   User.findAll()
     .then(users => res.json({
       status: 200,
       users: users
+    }))
+    .catch(err => res.json({
+      status: 400,
+      err: err
+    }))
+})
+
+// Get specific user by id.
+router.get('/users/:id', (req, res) => {
+  User.findAll({
+    where: { id: req.params.id }
+  })
+    .then(user => res.json({
+      status: 200,
+      user: user
+    }))
+    .catch(err => res.json({
+      status: 400,
+      err: err
+    }))
+})
+
+// Create a new user.
+router.post('/users', (req, res) => {
+  User.create(req.body)
+    .then(user => res.json({
+      status: 200,
+      user: user
     }))
     .catch(err => res.json({
       status: 400,
