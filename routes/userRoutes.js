483576e5b354edbd95a573acde0abab22b6f8d05
Steven Quintana <steve.e.quint@gmail.com>
2021-09-23 13:50:20 -0700

    Add user register and login routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,21 @@
 const router = require('express').Router()
 const { User } = require('../models')
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
+
+router.post('/users/register', (req, res) => {
+  User.register(new User({ username: req.body.username }), req.body.password, err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
+})
+
+router.post('/users/login', (req, res) => {
+  User.authenticate()(req.body.username, req.body.password, (err, user) => {
+    if (err) { console.log(err) }
+    res.json(user ? jwt.sign({ id: user.id }, process.env.SECRET) : null)
+  })
+})
 
 // Get all users.
 router.get('/users', (req, res) => {
