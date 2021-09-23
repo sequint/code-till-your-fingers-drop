de8ae5fb09081b6d0120fbf18bbea705791df859
Steven Quintana <steve.e.quint@gmail.com>
2021-09-22 17:07:24 -0700

    Use matched user's id in project post

--- public/assets/js/myProjects.js
+++ public/assets/js/myProjects.js
@@ -5,7 +5,7 @@ document.getElementById('createProject').addEventListener('click', event => {
   let userName = 'Sleepy Neko'
 
   // Find the matching current user in the user database, and get user information.
-  return axios.get(`/api/users`,)
+  axios.get(`/api/users`,)
     .then(({ data: payload }) => {
       let users = payload.users
       let userMatch = users.filter(user => user.username === userName)
@@ -13,29 +13,26 @@ document.getElementById('createProject').addEventListener('click', event => {
       return userMatch
 
     })
-    .then(userMatch => console.log(userMatch))
+    .then(userMatch => {
+      // Create data variable using values from form inputs.
+      projectName = event.target.parentNode.parentNode.children[1].children[0].children[0].children[1].value
+
+      description = event.target.parentNode.parentNode.children[1].children[0].children[1].children[1].value
+
+      startDate = event.target.parentNode.parentNode.children[1].children[0].children[7].children[0].value
+
+      // Create a post axios request to send new project information to the database.
+      axios.post('/api/projects', {
+        projectName: projectName,
+        description: description,
+        startDate: startDate,
+        userId: userMatch[0].id
+      })
+        .then(({ data: project }) => {
+          console.log(`${project.project.projectName} project created!`)
+        })
+        .catch(err => console.log(err))
+    })
     .catch(err => console.log(err))
-
-  // // Create data variable using values from form inputs.
-  // projectName = event.target.parentNode.parentNode.children[1].children[0].children[0].children[1].value
-  // console.log(projectName)
-
-  // description = event.target.parentNode.parentNode.children[1].children[0].children[1].children[1].value
-  // console.log(description)
-
-  // startDate = event.target.parentNode.parentNode.children[1].children[0].children[7].children[0].value
-  // console.log(startDate)
-
-  // // Create a post axios request to send new project information to the database.
-  // axios.post('/api/projects', {
-  //   projectName: projectName,
-  //   description: description,
-  //   startDate: startDate,
-  //   userId: 1
-  // })
-  //   .then(({data: project}) => {
-  //     console.log(`${project.project.projectName} project created!`)
-  //   })
-  //   .catch(err => console.log(err))
 
 })
