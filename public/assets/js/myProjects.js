e95c9572639976cded656354935192da67ebb762
Steven Quintana <steve.e.quint@gmail.com>
2021-09-22 20:15:06 -0700

    Create an object of project data and use that to pass to each function layer and be added to

--- public/assets/js/myProjects.js
+++ public/assets/js/myProjects.js
@@ -1,5 +1,26 @@
 
-const findCategoryAndSetData = (userMatch, categoryName) => {
+// Function that set all data recieved from the user.
+const setData = (userMatch, categoryMatch, projectName, description, startDate) => {
+
+  // Create a post axios request to send new project information to the database.
+  axios.post('/api/projects', {
+    projectName: projectName,
+    description: description,
+    startDate: startDate,
+    categoryId: categoryId,
+    userId: userId
+  })
+    .then(({ data: project }) => {
+      console.log(`${project.project.projectName} project created!`)
+    })
+    .catch(err => console.log(err))
+
+}
+
+// Function that tries to find a title match of user input to categories table.
+// If match is not found a new category is created and the function runs again.
+// If found, the function sends user and category data to the set data function.
+const findCategory = (categoryName, projectData) => {
 
   // Check if category typed in already exists.
   axios.get('/api/categories')
@@ -11,24 +32,22 @@ const findCategoryAndSetData = (userMatch, categoryName) => {
           title: categoryName
         })
           .then(({ data: payload }) => {
-            // Return new category created.
-            console.log(payload.category.title)
-            findCategoryAndSetData(userMatch, payload.category.title)
+            // Recursivly call findCategory with category title of new category made.
+            findCategory(payload.category.title, projectData)
           })
           .catch(err => console.log(err))
-
-        console.log({ newCategory })
-
       }
       else {
         // Return an array with the match category and user to the next then.
-        return [categoryMatch, userMatch]
+        return categoryMatch
       }
 
     })
-    .then(([categoryMatch, userMatch]) => {
-      console.log(categoryMatch)
-      console.log(userMatch)
+    .then(([ categoryMatch ]) => {
+      // Add the matched category's id to the project data object.
+      projectData.categoryId = categoryMatch.id
+      console.log(projectData)
+
     })
     .catch(err => console.log(err))
 
@@ -50,10 +69,18 @@ document.getElementById('createProject').addEventListener('click', event => {
 
     })
     .then(userMatch => {
-      // Create a variable for category input.
+
+      // Create an object of new project data input from user.
+      let projectData = {
+        projectName: event.target.parentNode.parentNode.children[1].children[0].children[0].children[1].value,
+        description: event.target.parentNode.parentNode.children[1].children[0].children[1].children[1].value,
+        startDate: event.target.parentNode.parentNode.children[1].children[0].children[7].children[0].value,
+        userId: userMatch[0].id
+      }
+      // Create data variable using values from form inputs.
       let categoryName = event.target.parentNode.parentNode.children[1].children[0].children[2].children[1].value
 
-      findCategoryAndSetData(userMatch, categoryName)
+      findCategory(categoryName, projectData)
       
       // // Check if category typed in already exists.
       // axios.get('/api/categories')
@@ -94,7 +121,6 @@ document.getElementById('createProject').addEventListener('click', event => {
       // startDate = event.target.parentNode.parentNode.children[1].children[0].children[7].children[0].value
 
       // userId = userMatch[0].id
-      // console.log(userId)
 
       // // Create a post axios request to send new project information to the database.
       // axios.post('/api/projects', {
