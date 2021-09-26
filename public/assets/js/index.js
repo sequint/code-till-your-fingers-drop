01dd2f39b82c823f7db1ae936e7c582295a41db9
Steven Quintana <steve.e.quint@gmail.com>
2021-09-26 09:47:58 -0700

    After setting vars, update the project's percent complete property

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,19 +1,26 @@
-
 // Function to update progress bar.
 const updateProgressBar = projectId => {
 
   axios.get(`/api/tasks/${projectId}`)
   .then(({ data: payload }) => {
-    console.log(payload.task)
+
+    // Create array of tasks filter to which ones are complete.
+    let completeTasks = payload.task.filter(tsk => tsk.isComplete === true)
+    // Create numberator and denominator based on task lengths.
+    let numerator = completeTasks.length
+    let denominator = payload.task.length
+
+    let percentComplete = (numerator / denominator) * 100
+
+    axios.put(`/api/projects/${projectId}`, {
+      percentComplete: percentComplete
+    }, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    })
+
   })
   .catch(err => console.log(err))
-
-      // Create array of tasks filter to which ones are complete.
-      let completeTasks = task.filter(tsk => tsk.isComplete === true)
-      // Create numberator and denominator based on task lengths.
-      let numerator = completeTasks.length()
-      let denominator = task.length()
-
-      let percentComplete = (numerator / denominator) * 100
 
 }
